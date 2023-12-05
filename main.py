e4b23ee950f496749afc27197f50773a047bd2ae
uumaaa <vma.iantonio@gmail.com>
2023-12-04 22:43:21 -0600

    act

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from Models import naive_bayes
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-model = naive_bayes.NaiveBayesClassifier()
-
 # Fijamos una semilla para reproducibilidad
 np.random.seed(42)
-
-# Generar datos de entrenamiento
-X = np.random.randn(200, 3)  # 100 ejemplos, 3 características
-y = np.random.choice([0, 1], size=200)  # Clases binarias (0 o 1)
-
-
-
+X = np.random.randn(200, 3) 
+y = np.random.choice([0], size=200)
+model = naive_bayes.NaiveBayesClassifier()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=40)
 model.fit(X_train,y_train)
-model.predict(X_test)
+y_predict = model.predict(X_test)
+print(y_predict)
+print(y_test)
+accuracy = accuracy_score(y_test,y_predict)
+print(accuracy)
