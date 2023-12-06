bffb491b4aed7097d216d4ec8c78c59f8dc05326
uumaaa <vma.iantonio@gmail.com>
2023-12-06 17:49:11 -0600

    commit por: MexboxLuis

--- Models/naive_bayes.py
+++ Models/naive_bayes.py
@@ -14,12 +14,10 @@ class NaiveBayesClassifier:
         self.prob_classes = np.zeros((len(unique_labels),1))
         self.covariance_matrices = np.zeros((len(unique_labels),n,n))
         for idx,instance in enumerate(X_train):
-            print(idx)
             data_in_class[label_transform[y_train[idx]]].append(instance)
         for idx,data in enumerate(data_in_class):
             data = np.array(data)
             self.means[idx] = np.mean(data,axis=0)
-            self.prob_classes[idx] = len(data) / n_data
             summ = np.zeros((n,n))
             for element in data:
                 summ  = summ + np.outer(element,element) - np.outer(self.means[idx],self.means[idx])
@@ -32,12 +30,11 @@ class NaiveBayesClassifier:
             max_d = -np.inf
             max_prob_class = -1
             for idx in range(len(self.means)):
-                prob_C = self.prob_classes[idx]
                 det_C = np.linalg.det(self.covariance_matrices[idx])
                 diff = (x-self.means[idx]).reshape((3,1))
                 mult_b = np.matmul(np.linalg.inv(self.covariance_matrices[idx]),diff)
                 mult = np.matmul(diff.T,mult_b)
-                d = np.log(prob_C) - (1/2)*np.log(det_C) - (1/2)*mult
+                d = - (1/2)*np.log(det_C) - (1/2)*mult[0][0]
                 if(d > max_d):
                     max_d = d
                     max_prob_class = idx
